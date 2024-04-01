d25b4967738da6977436408c3d9156ea829eb760
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-01 17:21:21 +0200

    feat(ui/main): Clean the MainActivity to only load inside a container, the new Note Fragment (+ adapt the Activity UI Xml).

--- app/src/main/java/com/openclassrooms/notes/ui/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/notes/ui/MainActivity.kt
@@ -2,14 +2,9 @@ package com.openclassrooms.notes.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.openclassrooms.notes.R
 import com.openclassrooms.notes.databinding.ActivityMainBinding
-import com.openclassrooms.notes.data.repository.NotesRepository
-import com.openclassrooms.notes.ui.widget.NoteItemDecoration
-import com.openclassrooms.notes.ui.widget.NotesAdapter
-import kotlinx.coroutines.launch
+import com.openclassrooms.notes.ui.noteUi.NoteFragment
 
 /**
  * The main activity for the app.
@@ -21,60 +16,19 @@ class MainActivity : AppCompatActivity() {
      */
     private lateinit var binding: ActivityMainBinding
 
-    private val notesAdapter = NotesAdapter(emptyList())
 
-    private val notesRepository = NotesRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        initRecyclerView()
-        initFABButton()
-        collectNotes()
-    }
-
-    /**
-     * Collects notes from the repository and updates the adapter.
-     */
-    private fun collectNotes() {
-        lifecycleScope.launch {
-            notesRepository.notes.collect {
-                notesAdapter.updateNotes(it)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_fragment_Container, NoteFragment.newInstance())
+                .commitNow()
         }
     }
 
-    /**
-     * Initializes the FAB button.
-     */
-    private fun initFABButton() {
-        binding.btnAdd.setOnClickListener {
-            MaterialAlertDialogBuilder(this).apply {
-                setTitle(R.string.coming_soon)
-                setMessage(R.string.not_available_yet)
-                setPositiveButton(android.R.string.ok, null)
-            }.show()
-        }
-    }
-
-    /**
-     * Initializes the RecyclerView.
-     */
-    private fun initRecyclerView() {
-        with(binding.recycler) {
-            addItemDecoration(
-                NoteItemDecoration(
-                    resources.getDimensionPixelSize(R.dimen.default_margin),
-                    resources.getInteger(R.integer.span_count)
-                )
-            )
-
-            adapter = notesAdapter
-        }
-
-    }
 
 }
