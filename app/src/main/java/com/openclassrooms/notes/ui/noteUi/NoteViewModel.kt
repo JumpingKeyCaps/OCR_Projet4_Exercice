68779edf7cd009fd7fa3ae71e6757e074dd4bdeb
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-02 00:24:38 +0200

    feat(ui/noteUi): add Dependency Injection in the note ViewModel constructor.

--- app/src/main/java/com/openclassrooms/notes/ui/noteUi/NoteViewModel.kt
+++ app/src/main/java/com/openclassrooms/notes/ui/noteUi/NoteViewModel.kt
@@ -3,31 +3,19 @@ package com.openclassrooms.notes.ui.noteUi
 import androidx.lifecycle.ViewModel
 import com.openclassrooms.notes.data.repository.NotesRepository
 import com.openclassrooms.notes.model.Note
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class NoteViewModel: ViewModel() {
+/**
+ * The Note ViewModel
+ *
+ * Use Hilt to inject the used NoteRepository by the constructor
+ *
+ */
+@HiltViewModel
+class NoteViewModel @Inject constructor(private val notesRepository: NotesRepository) : ViewModel() {
 
-    private var notesRepository: NotesRepository = NotesRepository()
-
-    /**
-     * Si on a besoin d'un contrôle spécifique sur la valeur actuelle de notre liste de notes dans le ViewModel
-     * on peut utiliser StateFlow , plus proche du fonctionement d'un livedata
-     *  ---> l'inconvenient est l'utilisation de 2 coroutines pour collecter nos 2 flow (une dans le fragment et une dans le viewmodel)
-     *
-     * ------------------------------------------------------------------------
-     * private val _notes = MutableStateFlow<List<Note>>(emptyList())
-     * val notes: StateFlow<List<Note>> = _notes.asStateFlow()
-     *
-     * init {
-     *   viewModelScope.launch {
-     *       notesRepository.notes.collect {
-     *           _notes.value = it
-     *       }
-     *   }
-     * }
-     *
-     * --------------------------------------------------------------------------
-     **/
 
     /**
      * Fetch the note list,
@@ -53,5 +41,26 @@ class NoteViewModel: ViewModel() {
      */
     fun removeNote(index: Int?, noteObject:Note?, title: String?) = notesRepository.removeNote(index,noteObject,title)
 
+
+
+    /**
+     * Si on a besoin d'un contrôle spécifique sur la valeur actuelle de notre liste de notes dans le ViewModel
+     * on peut utiliser StateFlow , plus proche du fonctionement d'un livedata
+     *  ---> l'inconvenient est l'utilisation de 2 coroutines pour collecter nos 2 flow (une dans le fragment et une dans le viewmodel)
+     *
+     * ------------------------------------------------------------------------
+     * private val _notes = MutableStateFlow<List<Note>>(emptyList())
+     * val notes: StateFlow<List<Note>> = _notes.asStateFlow()
+     *
+     * init {
+     *   viewModelScope.launch {
+     *       notesRepository.notes.collect {
+     *           _notes.value = it
+     *       }
+     *   }
+     * }
+     *
+     * --------------------------------------------------------------------------
+     **/
 
 }
