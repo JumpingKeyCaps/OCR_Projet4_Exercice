3463dce40952e4dd3b4b6fd5e72151c6ad0ad8ae
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-02 00:09:35 +0200

    feat(di): Create an Application class to setup @HiltAndroidApp to use DI

---
+++ app/src/main/java/com/openclassrooms/notes/di/NotesApplication.kt
@@ -0,0 +1,11 @@
+package com.openclassrooms.notes.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application Class to use Hilt
+ */
+@HiltAndroidApp
+class NotesApplication: Application() {
+}
