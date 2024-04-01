b2461bccbd1a8bccfbb4b05d61445f97b061254c
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-01 21:08:40 +0200

    feat(data/repository): Add  removeNote() method and addNote() method. - add Javadoc.

--- app/src/main/java/com/openclassrooms/notes/data/repository/NotesRepository.kt
+++ app/src/main/java/com/openclassrooms/notes/data/repository/NotesRepository.kt
@@ -19,7 +19,23 @@ class NotesRepository {
     /**
      * A flow that emits a list of all notes.
      */
-    val notes: Flow<List<Note>> = flow {
-        emit(notesApiService.getAllNotes())
-    }
+    val notes: Flow<List<Note>> = flow { emit(notesApiService.getAllNotes()) }
+
+    /**
+     * Add a note to the list of notes
+     * @param newNote The new note object to add to the list.
+     */
+    fun addNote(newNote:Note) = notesApiService.addNote(newNote)
+
+    /**
+     * Remove a note to the list of notes.
+     *
+     * Use one of the 3 parameters to remove a note object.
+     *
+     * @param index (optional) the index position of the note to remove.
+     * @param noteObject (optional) the note object reference to remove of the list.
+     * @param title (optional) the title of the note to remove.
+     */
+    fun removeNote(index: Int?, noteObject:Note?, title: String?) = notesApiService.removeNote(index,noteObject,title)
+
 }
