75de3cac3255caf05265e46a4286bceff8220753
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-03 13:27:49 +0200

    test(data/repository): Update NoteRepository unit tests with mockK and runTest().

--- app/src/test/java/com/openclassrooms/notes/dataUnitTest/NotesRepositoryTest.kt
+++ app/src/test/java/com/openclassrooms/notes/dataUnitTest/NotesRepositoryTest.kt
@@ -3,17 +3,15 @@ package com.openclassrooms.notes.dataUnitTest
 import com.openclassrooms.notes.data.repository.NotesRepository
 import com.openclassrooms.notes.data.service.NotesApiService
 import com.openclassrooms.notes.model.Note
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestCoroutineScope
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito.doNothing
-import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when`
-import org.mockito.MockitoAnnotations
 
 /**
  * Test class for the Notes Repository
@@ -24,7 +22,7 @@ class NotesRepositoryTest {
     /**
      * Mocked class
      */
-    @Mock
+    @MockK
     private lateinit var mockNotesApiService: NotesApiService
     private lateinit var notesRepository: NotesRepository
 
@@ -33,30 +31,33 @@ class NotesRepositoryTest {
      */
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this) // Initialize mocks before each test
+        MockKAnnotations.init(this)
         notesRepository = NotesRepository(mockNotesApiService)
 
     }
 
+    // New test with MockK et runTest()
 
     /**
      * Test the flow of notes list emits by the repository
      */
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `notes flow emits all notes from the api service`() {
-        val expectedNotes = listOf(Note("Titre1", "Contenu1"), Note("Titre2", "Contenu2"))
-        `when`(mockNotesApiService.getAllNotes()).thenReturn(expectedNotes.toMutableList())
+        //mocked data
+        val expectedNotes = listOf(Note("Titre1", "msg1"), Note("Titre2", "msg2"))
+        //mocked Api behavior
+        //use of coEvery function from mockK to define expectations for suspending functions like getAllNotes()
+        coEvery { mockNotesApiService.getAllNotes() } returns expectedNotes.toMutableList()
 
-        val capturedNotes = mutableListOf<List<Note>>()
-
-        val testCoroutineScope = TestCoroutineScope() // Use TestCoroutineScope for coroutines
-        testCoroutineScope.launch {
-            notesRepository.notes.collect { capturedNotes.add(it) } // Collect emitted values
+        //Running the test with coroutine scope
+        //notesRepository is a "cold flow" (emits values over time), this is why i use a coroutine to test it.
+        runTest {
+            val capturedNotes = mutableListOf<List<Note>>()
+            //Capturing emitted value
+            notesRepository.notes.collect { capturedNotes.add(it) }
+            //Assert: test expected value with the collected value across the flow.
+            assertEquals(expectedNotes, capturedNotes.first())
         }
-
-        assertEquals(expectedNotes, capturedNotes.first())
-
     }
 
     /**
@@ -64,27 +65,47 @@ class NotesRepositoryTest {
      */
     @Test
     fun `addNote calls notesApiService to add the note`() {
-        val newNote = Note("Titre3", "Contenu3")
+        //Creating Test Data
+        val newNote = Note("Titre3", "msg3")
 
-        doNothing().`when`(mockNotesApiService).addNote(newNote) // Use doNothing() for void method
+        //Mocking API behavior
+        //use of every function from MockK to define the behavior of the mocked mockNotesApiService for the addNote() function.
+        every { mockNotesApiService.addNote(newNote) } returns Unit
 
+        //act:
         notesRepository.addNote(newNote)
 
-        verify(mockNotesApiService).addNote(newNote) // Verify the call
+        // verifies that the mocked addNote function was called exactly once
+        // with the same newNote object that was passed during the test execution.
+        // This ensures the repository interacts with the API service as expected when adding a new note
+        verify(exactly = 1) { mockNotesApiService.addNote(newNote) }
+
     }
 
     /**
      * Test the removeNote method behavior.
      */
     @Test
-    fun `removeNote calls notesApiService with the provided index`() {
+    fun `removeNote removes the note at the given index from the repository`() {
+        //Creating Test Data
         val indexToRemove = 2
-        doNothing().`when`(mockNotesApiService).removeNote(indexToRemove, null, null) // Use doNothing() for void method
-
-        notesRepository.removeNote(indexToRemove, null, null)
-
-        verify(mockNotesApiService).removeNote(indexToRemove, null, null) // Verify call with index
+        val expectedNotes = mutableListOf(Note("Titre1", "msg1"), Note("Titre2", "msg2"))
+        //Mocking API behavior
+        coEvery { mockNotesApiService.getAllNotes() } returns expectedNotes.toMutableList()
+        coEvery { mockNotesApiService.removeNote(indexToRemove, null, null) } returns Unit
+        //Running the test with coroutine scope
+        runTest {
+            notesRepository.removeNote(indexToRemove, null, null)
+            notesRepository.notes.collect {
+                // Assert to check if the note was removed
+                val actualSize = expectedNotes.size
+                val subList1 = expectedNotes.subList(0, indexToRemove.coerceAtMost(actualSize))
+                val subList2 = expectedNotes.subList((indexToRemove + 1).coerceAtMost(actualSize), actualSize)
+                assertEquals(subList1 + subList2, it)
+            }
+        }
     }
+
 
 
 }
